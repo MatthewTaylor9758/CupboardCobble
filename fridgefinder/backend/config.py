670c5459162127ce69a8469679639972fba248bc
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-02-11 17:10:19 -0500

    create database, test_user, and add database url to config file

--- fridgefinder/backend/config.py
+++ fridgefinder/backend/config.py
@@ -3,3 +3,4 @@ import os
 class Config(object):
     GREETING = 'Salutations, superior students!'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-for-devs'
+    SQLALCHEMY_DATABASE_URI = postgresql://test_user:password@localhost/fridge_world
