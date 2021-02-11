90b1a4b91a5294cab67655d0a9eea7c24021c917
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-02-11 18:33:29 -0500

    add user model without password(to be added later)

--- fridgefinder/backend/app/models.py
+++ fridgefinder/backend/app/models.py
@@ -4,3 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 # create the SQLAlchemy object and save it as db
 db = SQLAlchemy()
+
+def Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    # password to come tomorrow when I go over werkzeug security and password hashing
