62232b6dae44f6b9a91f08e9658b3cb39560a066
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-02-12 16:03:19 -0500

    add preliminary users table

--- fridgefinder/backend/app/models.py
+++ fridgefinder/backend/app/models.py
@@ -5,8 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 # create the SQLAlchemy object and save it as db
 db = SQLAlchemy()
 
-def Users(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     # password to come tomorrow when I go over werkzeug security and password hashing
+
+class Foods(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    amount = db.Column(db.String, nullable=False)
