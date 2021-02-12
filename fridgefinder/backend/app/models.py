fe9859380182ad1f78cc132a4b0fef436a3f5f25
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-02-12 16:08:45 -0500

    add prliminary pantries table

--- fridgefinder/backend/app/models.py
+++ fridgefinder/backend/app/models.py
@@ -15,3 +15,9 @@ class Foods(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
     amount = db.Column(db.String, nullable=False)
+
+class Pantry(db.Model):
+    __tablename__ = 'Pantries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, unique=True)
