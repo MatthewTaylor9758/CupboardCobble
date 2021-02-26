53fcc915d1bed415bedaf2b87090d57dc4888a4d
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-02-25 19:43:43 -0500

    get front end connected to back end

--- fridgefinder/backend/app/__init__.py
+++ fridgefinder/backend/app/__init__.py
@@ -13,30 +13,61 @@ app.config.from_object(Config)
 db.init_app(app)
 Migrate(app, db)
 
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        print("favicon route_____")
+        return app.send_static_file('favicon.ico')
+    print("index route_____")
+    return app.send_static_file('index.html')
+
+# The above is apparently how you tie the flask server to the react-redux front end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # db = SQLAlchemy(app) (don't need this for the same reason as on line 3-5)
 
-print('the secret key is: ', app.config['SECRET_KEY'])
+# print('the secret key is: ', app.config['SECRET_KEY'])
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return '<h1>Home</h1>'
+# @app.route('/')
+# @app.route('/home')
+# def home():
+#     return '<h1>Home</h1>'
 
-@app.route('/about')
-def about():
-    return '<h1>About</h1>'
+# @app.route('/about')
+# def about():
+#     return '<h1>About</h1>'
 
-@app.route('/item/<int:id>')
-def item(id):
-    return f'<h1>Item {id}</h1>'
+# @app.route('/item/<int:id>')
+# def item(id):
+#     return f'<h1>Item {id}</h1>'
 
-@app.before_first_request
-def before_first_request_function():
-    print('before_first_request happens once.')
+# @app.before_first_request
+# def before_first_request_function():
+#     print('before_first_request happens once.')
 
-@app.before_request
-def before_request_function():
-    print('before_request is running properly.')
+# @app.before_request
+# def before_request_function():
+#     print('before_request is running properly.')
 
 # @app.after_request
 # def after_request_function():
