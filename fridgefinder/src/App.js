4cba002ba4b83b3059e682cb028bf6dfbe2c7faa
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-03-07 17:27:59 -0500

    add basic splash page and set up for it

--- fridgefinder/src/App.js
+++ fridgefinder/src/App.js
@@ -1,46 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SplashPage from './pages/SplashPage';
 
 function App() {
   /*
   Will be making my own proxy in Flask when I get there, but for now I am using the
   one I have listed on the line 10(cors-anywhere.herokuapp.com)
   */
-  const makeRequest = async () => {
-    let res = await fetch('https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3', {
-      method: 'get',
-      // mode: '*cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
+  // const makeRequest = async () => {
+  //   let res = await fetch('https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3', {
+  //     method: 'get',
+  //     // mode: '*cors',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
 
-    })
-    let response = await res.json();
-    console.log(response);
-  }
+  //   })
+  //   let response = await res.json();
+  //   console.log(response);
+  // }
 
   return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      <div>
-        <button onClick={makeRequest}>Make Request</button>
-      </div>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={SplashPage}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ fridgefinder/src/pages/SplashPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+
+function SplashPage () {
+  return (
+    <>
+      <h1>
+        This is the splash page.
+      </h1>
+    </>
+  )
+}
+export default SplashPage;
