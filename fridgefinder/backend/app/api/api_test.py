4086b399b7b8812e2febd0a3763677ac37dc136e
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-03-08 16:03:19 -0500

    get alternative proxy working

--- fridgefinder/backend/app/api/api_test.py
+++ fridgefinder/backend/app/api/api_test.py
@@ -5,6 +5,6 @@ api_test = Blueprint('tests', __name__)
 
 @api_test.route('/')
 def test_api():
-    req = requests.get('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3')
+    req = request.get('http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3')
     res = req.json()
     print(res)
--- fridgefinder/src/pages/SplashPage.js
+++ fridgefinder/src/pages/SplashPage.js
@@ -8,12 +8,25 @@ function SplashPage () {
     let res = await fetch('/api/tests')
   }
 
+  const makeOtherRequest = async () => {
+    let res = await fetch('https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3', {
+      method: 'get',
+      // mode: '*cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    let res2 = await res.json();
+    console.log(res2);
+  }
+
   return (
     <>
       <h1>
         This is the splash page.
       </h1>
-      <button onClick={makeRequest}>Make Request</button>
+      <button onClick={makeRequest}>Make My-Proxy Request</button>
+      <button onClick={makeOtherRequest}>Make Other-Proxy Request</button>
     </>
   )
 }
