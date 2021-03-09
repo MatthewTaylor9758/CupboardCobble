fe6cac0adda5743255596e4d42c90f7c8c6d7e65
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-03-09 17:37:41 -0500

    add some intiial styling to navbar

--- fridgefinder/src/tools/Navbar.js
+++ fridgefinder/src/tools/Navbar.js
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { makeStyles, Card, Button, Typography, AppBar, Toolbar, Grid, Modal } from '@material-ui/core';
+import '../CSS/NavBar.css';
 
 function NavBar() {
   return (
     <>
-      <AppBar>
+      <AppBar id='navbar'>
         <Toolbar>
           <Grid container xs={12}>
-            <Grid item xs={4}>
+            <Grid item xs={4} id='navbar_left'>
               This is the left side
             </Grid>
             <Grid item xs={4}>
-              <NavLink to={'/'} id='nav_logo'>FridgeFinder</NavLink>
+              <NavLink to={'/'} id='navbar_logo'>FridgeFinder</NavLink>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} id='navbar_right'>
               This is the right side
             </Grid>
           </Grid>
