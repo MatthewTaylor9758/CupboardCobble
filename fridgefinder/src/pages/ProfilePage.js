7e60a138c62962883627651ab249593c79530ee9
Matthew Taylor <matthewtaylor9758@gmail.com>
2021-03-15 17:09:06 -0400

    set up recipe grid

--- fridgefinder/src/pages/ProfilePage.js
+++ fridgefinder/src/pages/ProfilePage.js
@@ -13,6 +13,41 @@ function ProfilePage() {
         </Grid>
         <Grid item xs={9} id='recipe_field'>
           <Typography variant={'h4'}>Make your Meal</Typography>
+          <div id='recipes'>
+            <div id='recipe-row'>
+              <div id='recipe-item'>
+                Item 1
+              </div>
+              <div id='recipe-item'>
+                Item 2
+              </div>
+              <div id='recipe-item'>
+                Item 3
+              </div>
+            </div>
+            <div id='recipe-row'>
+              <div id='recipe-item'>
+                Item 4
+              </div>
+              <div id='recipe-item'>
+                Item 5
+              </div>
+              <div id='recipe-item'>
+                Item 6
+              </div>
+            </div>
+            <div id='recipe-row'>
+              <div id='recipe-item'>
+                Item 7
+              </div>
+              <div id='recipe-item'>
+                Item 8
+              </div>
+              <div id='recipe-item'>
+                Item 9
+              </div>
+            </div>
+          </div>
         </Grid>
       </Grid>
     </>
